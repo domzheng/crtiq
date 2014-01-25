4e2f3e4a8c57a641bfad582ad1987bf3139874f4
Dominick <dominickzheng@gmail.com>
2014-01-25 17:58:14 -0500

    fixed the about page made it pretty n shit

--- templates/about_screen.php
+++ templates/about_screen.php
@@ -26,30 +26,31 @@
     </div>
 
         <h1 class="credits"> designed & developed by </h1>
-        <h2 class="ampersand"> & </h2>
+        <div class="aboutfade">
+            <h2 class="ampersand"> & </h2>
 
-        <div class="leftabout">
-            <h1> Daniel Yue </h1>
-            <h2> Baltimore, MD </h2>
-            <img class="aboutthumb" src="img/harrypotterthumb.png">
-            <p> Daniel Yue is a sophomore in Lowell House. <br>
-            He is studying physics. <br>
-            He loves photography, design, and music<br>
-            and of course long walks on the beach.<br>
-            You can reach him at danielyue at college.harvard.edu. </p>
+            <div class="leftabout">
+                <h1> Daniel Yue </h1>
+                <h2> Baltimore, MD </h2>
+                <img class="aboutthumb" src="img/harrypotterthumb.png">
+                <p> Daniel Yue is a sophomore in Lowell House. <br>
+                He is studying physics. <br>
+                He loves photography, design, and music<br>
+                and of course long walks on the beach.<br>
+                You can reach him at danielyue at college.harvard.edu. </p>
+            </div>
+            
+            <div class="rightabout">
+                <h1> Dominick Zheng </h1>
+                <h2> Boston, MA </h2>
+                <img class="aboutthumb" src="img/spedthumb.png">
+                <p> Dominick Zheng is also a sophomore in Lowell House. <br>
+                He is studying HDRB. <br>
+                Good design, hip hop, and landscapes<br>
+                get him hot and bothered.<br>
+                You can reach him at dominickzheng at college.harvard.edu. </p>
+            </div>
         </div>
-        
-        <div class="rightabout">
-            <h1> Dominick Zheng </h1>
-            <h2> Boston, MA </h2>
-            <img class="aboutthumb" src="img/spedthumb.png">
-            <p> Dominick Zheng is also a sophomore in Lowell House. <br>
-            He is studying HDRB. <br>
-            Good design, hip hop, and landscapes<br>
-            get him hot and bothered.<br>
-            You can reach him at dominickzheng at college.harvard.edu. </p>
-        </div>
-
         <div id="aboutfooter">
             For comments & critiques, contact admin@crtiq.com. This was CS50.
         </div>        
